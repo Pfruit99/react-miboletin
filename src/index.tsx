03e270c1d41cd9fc5c732df82272202d6d9d49ef
Pfruit99 <mora099999@gmail.com>
2023-04-08 14:36:13 -0500

    build: :zap: implementacion de tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,80 @@
+// react 17.0.2
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <App/>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// to import
+// {
+//   "name": "reacttemplate",
+//   "version": "0.1.0",
+//   "private": true,
+//   "dependencies": {
+//     "@testing-library/jest-dom": "^5.16.5",
+//     "@testing-library/react": "^13.4.0",
+//     "@testing-library/user-event": "^13.5.0",
+//     "axios": "^0.26.0",
+//     "bootstrap": "^5.3.0-alpha1",
+//     "copy-webpack-plugin": "^10.2.4",
+//     "css-loader": "^6.6.0",
+//     "mini-css-extract-plugin": "^2.5.3",
+//     "react": "^17.0.2",
+//     "react-bootstrap": "^2.7.2",
+//     "react-dom": "^17.0.2",
+//     "react-router-dom": "^6.4.4",
+//     "react-scripts": "5.0.1",
+//     "redux": "^4.1.2",
+//     "sass": "^1.49.9",
+//     "sass-loader": "^12.6.0",
+//     "style-loader": "^3.3.1",
+//     "uglifyjs-webpack-plugin": "^2.2.0",
+//     "web-vitals": "^2.1.4"
+//   },
+//   "scripts": {
+//     "start": "react-scripts start",
+//     "build": "react-scripts build",
+//     "test": "react-scripts test",
+//     "eject": "react-scripts eject"
+//   },
+//   "eslintConfig": {
+//     "extends": [
+//       "react-app",
+//       "react-app/jest"
+//     ]
+//   },
+//   "browserslist": {
+//     "production": [
+//       ">0.2%",
+//       "not dead",
+//       "not op_mini all"
+//     ],
+//     "development": [
+//       "last 1 chrome version",
+//       "last 1 firefox version",
+//       "last 1 safari version"
+//     ]
+//   },
+//   "devDependencies": {
+//     "@babel/plugin-transform-runtime": "^7.17.0",
+//     "@types/jest": "^29.5.0",
+//     "@types/node": "^18.15.11",
+//     "@types/react": "^18.0.31",
+//     "@types/react-dom": "^18.0.11",
+//     "node-sass": "^8.0.0",
+//     "typescript": "^4.9.5"
+//   },
+//   "engines": {
+//     "node": ">=0.10.3 <17"
+//   }
+// }
+
+
